d3a764cc06a60bad6e83c70c840c3df97d378426
Anthony Edward Del Pozo Matias <anthonyedwarddelpozomatias@Anthonys-MacBook-Pro.local>
2018-11-13 03:14:54 -0500

    Heroku Configuration Files added

--- GEAPApi/settings.py
+++ GEAPApi/settings.py
@@ -124,7 +124,17 @@ TIME_ZONE = 'America/Bogota'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# LOGIN_REDIRECT_URL = '/account/home'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn asesoria.wsgi --log-file -
